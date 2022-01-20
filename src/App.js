08ad0f36869cb183c0e1d528ad8860ff926ba9a7
NaderMakram <nadermakram98@gmail.com>
2022-01-20 22:51:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Nav from './Nav';
+
+// pages
+import movies from './pages/movies'
+import favorites from './pages/Favorites'
+import login from './pages/login'
+import register from './pages/register'
+import SingleMovie from './pages/SingleMovie';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+    <Nav/>
+    <Switch>
+      <Route exact path={"/"} component={movies}/>
+      <Route path={"/favorites"} component={favorites}/>
+      <Route path={'/login'} component={login}/>
+      <Route path={'/register'} component={register}/>
+      <Route path={'/movie/:id?'} component={SingleMovie}/>
+    </Switch>
+  </BrowserRouter>
   );
 }
 
